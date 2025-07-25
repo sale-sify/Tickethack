d82db22e0491e116f4a367c96a8d5ce3d5999e28
Romain Sales <romain.sales1@gmail.com>
2025-07-25 12:05:32 +0200

    fix: remove hardcoded Aiven password

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +13,7 @@ router.get('/', function(req, res, next) {
 // Connexion avec la base de donnees 
 const { Client } = require('pg');
 const connectionString = new Client({
-connectionString: 'postgresql://neondb_owner:npg_h7rqO3NQtpuC@ep-wild-mountain-abc0qrca-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+connectionString: process.env.DATABASE_URL,
 ssl: { rejectUnauthorized: false }
 });
 const client = new Client( connectionString );
--- script.js
+++ script.js
@@ -5,32 +5,43 @@ const btnSearch = document.querySelector('.btn-search');
 
 btnSearch.addEventListener('click', 
     function() {
-        const urlTickets = `https://localhost:3001/${departure}/${arrival}/${date}`
+        const urlTickets = `http://localhost:3001/${departure}/${arrival}/${date}`
          fetch(urlTickets)
                 .then(response => response.json())
                 .then((data) => {
-            console.log(data.departure, data.arrival, data.date, data.price);
-            const resDeparture = data.departure
-            const resArrival = data.arrival
-            const resDate = data.date /*Faire une fonction pour sortir l'heure*/
-            const resPrice = data.price
-            
-            if (date.departure) {
-                document.querySelector('right1').innerHTML+=`
-                    <div>
-                        <p>${resDeparture}</p>
-                        <p>${resArrival}</p>
-                        <p>${resPrice}</p>
-                        <p>${resDate}</p>
-                    </div>`
-            } else {
-                    document.querySelector('right1').innerHTML+=`
-                    <div>
-                        <img src="/Images/notfound.png" alt="No result image Tickethack">
-                        <p>No result Found.</p>
-                    </div>`
-            }
-                })
-        document.querySelector('.right').style.display = 'none';
-        document.querySelector('.right1').style.display = 'flex';    
+                    if (data.length > 0) {
+                        for (let i = 0; i < data.length; i++) {
+                            
+                            console.log(data[i].departure, data[i].arrival, data[i].date, data[i].price);
+
+                            const resDeparture = data[i].departure;
+                            const resArrival = data[i].arrival;
+
+                            const resDate = function time (date) {
+                                date = new Date(data[i].date);
+                                return date.getHours
+                            }; 
+
+                            const resPrice = data[i].price;
+
+                            document.querySelector('right1').innerHTML+=`
+                                        <div>
+                                            <p>${resDeparture} > ${resArrival}</p>
+                                            <p>${resDate}</p>
+                                            <p>${resPrice}</p>  
+                                        </div>`;
+                        } 
+                    } else {
+                        document.querySelector('right1').innerHTML+=`
+                            <div>
+                                <img src="/Images/notfound.png" alt="No result image Tickethack">
+                                <p>No result Found.</p>
+                            </div>`;
+                    };
+            document.querySelector('.right').style.display = 'none';
+            document.querySelector('.right1').style.display = 'flex';
+            document.querySelector('.right1 > div').style.display = 'flex';    
+        });
     });
+
+   
